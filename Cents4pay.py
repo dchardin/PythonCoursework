13484b6048cdbacf4ab0c0eb4c049e167dae9ddf
dchardin <dch84121@gmail.com>
2016-04-26 19:30:17 -0400

    Corrected bug that was causing incorrect final total.

--- Cents4pay.py
+++ Cents4pay.py
@@ -1,38 +1,40 @@
 #!/bin/python
 
-
 # -----------------------------------------------------------------------------
 # Cents4pay.py
 # By dchardin <donnie@fedoraproject.org>
 # -----------------------------------------------------------------------------
 # Instructions
 # -----------------------------------------------------------------------------
-#
-# Design a program that calculates the amount of money a person would earn
-# over a period of time if his/her salary is one penny the first day, 
-# two pennies the second day, and continues to double each day. 
-# The program should ask the user for the number of days. Display a table 
-# showing what salary was for each day, and then show the total pay at the 
-# end of the period. The output should be displayed in a dollar amount, 
-# not the number of pennies. 
-#
-# For example if user enters 5 for total days worked, the output should be 
-# similar as:
-#
-# Day                        Daily Pay
-#
-# 1                             $0.01
-#
-# 2                             $0.02
-#
-# 3                             $0.04
-#
-# 4                             $0.08
-#
-# 5                             $0.16
-#
-# Total Pay			$0.31 
 
+'''
+ 
+Design a program that calculates the amount of money a person would earn
+over a period of time if his/her salary is one penny the first day, 
+two pennies the second day, and continues to double each day. 
+The program should ask the user for the number of days. Display a table 
+showing what salary was for each day, and then show the total pay at the 
+end of the period. The output should be displayed in a dollar amount, 
+not the number of pennies. 
+
+For example if user enters 5 for total days worked, the output should be 
+similar as:
+
+Day                        Daily Pay
+
+1                             $0.01
+
+2                             $0.02
+
+3                             $0.04
+
+4                             $0.08
+
+5                             $0.16
+
+Total Pay			$0.31 
+
+'''
 
 # Note: If any employer offers you this arrangement, TAKE IT!!!!!!!
 
@@ -64,12 +66,12 @@ totalearnings = 0
 
 daystowork = float(input("How many days would you like to work?: "))
 
-counter += 1
 
 print "On day %s" % day
 print "Your earnings are: "'${:,.2f}'.format(earnings)
 
 
+counter += 1
 day = counter
 
 earnings = salary
@@ -77,6 +79,7 @@ earnings = salary
 print "On day %s" % day
 print "Your earnings are: "'${:,.2f}'.format(earnings)
 
+totalearnings = totalearnings + earnings
 
 while counter != daystowork:
     counter += 1
